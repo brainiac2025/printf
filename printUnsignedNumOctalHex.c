bb5de81c5e8a763e6035192ce4282b87c3aad390
brainiac2025 <afeez.pierscon@gmail.com>
2023-08-22 16:07:05 -0700

    new changes added

--- printUnsignedNumOctalHex.c
+++ printUnsignedNumOctalHex.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * count_hex_digit - function to count hex digit
- * @num: the number pass
- * Return: int
- */
-unsigned int count_hex_digit(unsigned int num)
-{
-	unsigned int count = 0;
-
-	while (num != 0)
-	{
-		num /= 16;
-		count++;
-	}
-	return (count);
-}
-/**
  * print_octal - function to print unsigned int
  * @argument: number to print
  * @flags: flag specifier
@@ -75,76 +59,58 @@ int print_unsigned_int(va_list argument, flag *flags)
 /**
  * print_hex_lower - function to print hexadecimal in lowercase
  * @argument: decimal number entered
- * @flag: flag specifier
+ * @flags: flag specifier
  * Return: void
  */
-int print_hex_lower(va_list argument, flag *flag)
+int print_hex_lower(va_list argument, flag *flags)
 {
-	unsigned int num = va_arg(argument, unsigned int);
-	unsigned int i, base, remain;
-	unsigned int digitlen = count_hex_digit(num);
-	char *myhex = malloc(sizeof(char) * (digitlen + 1));
+	unsigned long num;
+	char *string;
 	int count = 0;
-	(void)flag;
 
-	if (myhex == NULL)
-		return (0);
+	if (flags->l_modifier)
+		num = (unsigned long)va_arg(argument, unsigned long);
+	else if (flags->h_modifier)
+		num = (unsigned short int)va_arg(argument, unsigned int);
+	else
+		num = (unsigned int)va_arg(argument, unsigned int);
 
-	base = 16;
+	string = to_base(num, 16, CHANGE_UNSIGNED | CONVERT_HEX_CASE, flags);
 
-	for (i = digitlen; num > 0; i--)
+	if (flags->hash && num)
 	{
-		remain = num % base;
-
-		if (remain < 10)
-			myhex[i] = (remain + '0');
-		else
-			myhex[i] = ((remain - 10 + 'a'));
-		num /= base;
+		*--string = 'x';
+		*--string = '0';
 	}
-	for (i = 0; i <= digitlen; i++)
-	{
-		count += _putchar(myhex[i]);
-	}
-	free(myhex);
-	return (count);
+	flags->unsign = 1;
+	return (count += display_number(string, flags));
 }
 
 /**
  * print_hex_upper - function to print hexadecimal in upper
  * @argument: decimal number entered
- * @flag: flag specifier
+ * @flags: flag specifier
  * Return: void
  */
-int print_hex_upper(va_list argument, flag *flag)
+int print_hex_upper(va_list argument, flag *flags)
 {
-	unsigned int num = va_arg(argument, unsigned int);
-	unsigned int i, base, remain;
-	unsigned int digitlen = count_hex_digit(num);
-	char *myhex = malloc(sizeof(char) * (digitlen + 1));
+	char *string;
 	int count = 0;
-	(void)flag;
+	unsigned int num;
 
-	if (myhex == NULL)
-		return (0);
+	if (flags->l_modifier)
+		num = (unsigned long)va_arg(argument, unsigned long);
+	else if (flags->h_modifier)
+		num = (unsigned short int)va_arg(argument, unsigned int);
+	else
+		num = (unsigned int)va_arg(argument, unsigned int);
 
-	base = 16;
-
-	for (i = digitlen; num > 0; i--)
+	string = to_base(num, 16, CHANGE_UNSIGNED, flags);
+	if (flags->hash && num)
 	{
-		remain = num % base;
-
-		if (remain < 10)
-			myhex[i] = (remain + '0');
-		else
-			myhex[i] = ((remain - 10 + 'A'));
-		num /= base;
+		*--string = 'X';
+		*--string = '0';
 	}
-
-	for (i = 0; i <= digitlen; i++)
-	{
-		count += _putchar(myhex[i]);
-	}
-	free(myhex);
-	return (count);
+	flags->unsign = 1;
+	return (count += display_number(string, flags));
 }
